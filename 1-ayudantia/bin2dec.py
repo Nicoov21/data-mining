c82094fb7055778de2b721e4ffb2298d05c1b999
Nicoov21 <nicolas.lagos.acevedo1@gmail.com>
2025-08-27 20:23:24 -0400

    Implementación de la funcion dec_to_bin, que hace de un número decimal a binario.

--- 1-ayudantia/bin2dec.py
+++ 1-ayudantia/bin2dec.py
@@ -9,4 +9,13 @@ def bin_to_dec(s: str) -> int:
         valorDecimal += bit * (2**numBin) #? Suma el valor del bit elevandolo por su posición
     return valorDecimal
 
-print(bin_to_dec('100'))
+def dec_to_bin(s: int) -> str:
+    if s == 0: #! Si el número es 0, retorna un mensaje advirtiendo que no debe ser 0
+        return ("El decimal no puede ser 0")
+    numeroBinario = '' #! Se inicializa la variable que contendrá la transformación del número decimal a binario
+    while s > 0: #! Se crea un bucle que se repite hasta que el número decimal sea 0
+        residuoDecimal = s % 2 #? Obtiene el restante de dividir el decimal por 2
+        numeroBinario = str(residuoDecimal) + numeroBinario #? Se concatena el restante al inicio del string
+        s = s // 2 #? Se actualiza el valor del decimal por su divisíon entera por 2
+    return numeroBinario #! Retorna el número en binario
+
